115d8cf284ec0c3a0d41d29d4559ae8e161efb47
Soumili Dey <73055873+soumilidey02@users.noreply.github.com>
2020-11-30 13:19:09 +0530

    Add files via upload

---
+++ sling.js
@@ -0,0 +1,31 @@
+class Sling {
+    constructor(bodyA, pointB) {
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 50
+        }
+        this.sling = Constraint.create(options);
+        this.pointB = pointB;
+        World.add(world, this.sling);
+    }
+    display(color) {
+        if (this.sling.bodyA) {
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            //fill(orange);
+            stroke(color);
+            strokeWeight(3);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            pop();
+        }
+    }
+    fly(){
+        this.sling.bodyA = null;
+    }
+    attach(body){
+        this.sling.bodyA = body;
+    }
+}
